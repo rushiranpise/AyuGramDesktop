35939f77aaa04ef1a8d18b5e3d1a00a66f8c12f8
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2023-07-03 02:37:31 +0530

    fix compile error

--- Telegram/SourceFiles/settings/settings_main.cpp
+++ Telegram/SourceFiles/settings/settings_main.cpp
@@ -68,7 +68,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include <QtGui/QClipboard>
 #include <QtGui/QWindow>
 
-#include "ayu/settings/settings_ayu.h"
+#include "ayu/ui/settings/settings_ayu.cpp"
 
 namespace Settings {
 namespace {
