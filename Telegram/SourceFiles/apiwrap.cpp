3f74919ba6d3ed24328861c84a09275763a807cf
ZavaruKitsu <alexeyzavar@gmail.com>
2023-09-29 13:45:13 +0300

    fix: one more condition for back from archived chats with hide all chats
    Co-authored-by: c0re100 <corehusky@gmail.com>

--- Telegram/SourceFiles/apiwrap.cpp
+++ Telegram/SourceFiles/apiwrap.cpp
@@ -470,7 +470,18 @@ void ApiWrap::toggleHistoryArchived(
 		if (archived) {
 			history->setFolder(_session->data().folder(archiveId));
 		} else {
-			history->clearFolder();
+			const auto settings = &AyuSettings::getInstance();
+			if (settings->hideAllChatsFolder) {
+				if (const auto window = Core::App().activeWindow()) {
+					if (const auto controller = window->sessionController()) {
+						const auto filters = &_session->data().chatsFilters();
+						const auto lookup_id = filters->lookupId(controller->session().premium() ? 0 : 1);
+						controller->setActiveChatsFilter(lookup_id);
+					}
+				}
+			} else {
+				history->clearFolder();
+			}
 		}
 		if (const auto data = _historyArchivedRequests.take(history)) {
 			data->second();
