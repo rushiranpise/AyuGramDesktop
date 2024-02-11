af18da81f4d3c03227e8b5459f52dd2f71f26f41
ZavaruKitsu <alexeyzavar@gmail.com>
2024-02-12 00:39:01 +0300

    fix: crash

--- Telegram/SourceFiles/storage/storage_sparse_ids_list.cpp
+++ Telegram/SourceFiles/storage/storage_sparse_ids_list.cpp
@@ -62,7 +62,9 @@ SparseIdsList::AddResult SparseIdsList::addRangeItemsAndCountNew(
 		SparseIdsSliceUpdate &update,
 		const Range &messages,
 		MsgRange noSkipRange) {
-	Expects(noSkipRange.from <= noSkipRange.till);
+	if (noSkipRange.from <= noSkipRange.till) {
+		return { 0 }; // AyuGram: fix crash when using `saveDeletedMessages`
+	}
 
 	if (noSkipRange.from == noSkipRange.till
 		&& std::begin(messages) == std::end(messages)) {
